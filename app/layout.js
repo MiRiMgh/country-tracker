649076949420c6beab7bb12ea09aa4cc183f45cb
MiRiMgh <gdmirim@gmail.com>
2026-07-05 15:44:40 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,13 @@
 
+export const metadata = {
+  title: "Мой паспорт мира",
+  description: "Отмечай страны, которые уже посетил",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
